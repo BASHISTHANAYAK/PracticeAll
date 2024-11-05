5642878b94e4e19a37317563ec73b011a3e6389b
BASHISTHANAYAK <bashisthanayak09@gmail.com>
2024-11-06 01:33:13 +0530

    a simple exer on it to get input

--- NodeJs/Accessing_InputAndPrint/takeInput.js
+++ NodeJs/Accessing_InputAndPrint/takeInput.js
@@ -1,16 +1,35 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-// function
+// // function
 
-rl.question("enter your question:", (input) => {
-  try {
-    console.log("input-", typeof input, input);
-  } catch (error) {
-    console.log(error);
-  }
-  rl.close();
-});
+// rl.question("enter your question:", (input) => {
+//   // if in input we write close it write close.
+//   if (input === "close") {
+//     rl.close();
+//   } else {
+//     try {
+//       const res = eval(input);
+//       console.log(`input is :- ${input}/n result is:- ${res}`);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//     rl.close();
+//   }
+// });
+
+
+// a simple exe to get input 
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// rl.question("enter num:-", (input) => {
+//   console.log("input-", input);
+//   rl.close();
+// });
