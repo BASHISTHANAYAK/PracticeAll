5e925620a27ed6041a251774957334cae563344d
BASHISTHANAYAK <bashisthanayak09@gmail.com>
2024-11-07 16:42:32 +0530

    adding a small css and js file in index.ejs

--- NodeJS_In-Sheryians_Coding_School/Initializing_project_Part6/index.js
+++ NodeJS_In-Sheryians_Coding_School/Initializing_project_Part6/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
-
+const path = require('path')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// its generally means to find the static files go to this public path.
+app.use(express.static(path.join(__dirname,'public')))
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
