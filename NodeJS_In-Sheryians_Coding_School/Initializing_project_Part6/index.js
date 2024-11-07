a0e0ad442f91c452735674676a0710d3cecf2e67
BASHISTHANAYAK <bashisthanayak09@gmail.com>
2024-11-07 17:09:20 +0530

    creating dynamic routing.

--- NodeJS_In-Sheryians_Coding_School/Initializing_project_Part6/index.js
+++ NodeJS_In-Sheryians_Coding_School/Initializing_project_Part6/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
-const path = require('path')
+const path = require("path");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // its generally means to find the static files go to this public path.
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   res.render("index");
+});
+
+app.get("/bashistha/:userName", function (req, res) {
+    let user = req.params.userName
+  res.send(`welcome ${user}`);
 });
 
 app.listen(3000, function () {
