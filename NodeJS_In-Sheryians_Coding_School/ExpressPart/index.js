0b3d3ee84087e566e11dfed0bb485cfef000d36b
BASHISTHANAYAK <bashisthanayak09@gmail.com>
2024-11-07 14:24:14 +0530

    creating a simple middleware

--- NodeJS_In-Sheryians_Coding_School/ExpressPart/index.js
+++ NodeJS_In-Sheryians_Coding_School/ExpressPart/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
-
 const app = express();
+
+// adding a simple middleware
+app.use(function (req, res, next) {
+  console.log("passed by middleware ...");
+  next();
+});
 
 app.get("/", function (req, res) {
   res.send("Hi i am bashistha nayak , i am a software developer ,Thanks");
 });
 
+//creatingn  a /about route
+app.get("/about", function (req, res) {
+  res.send("This is /about page.");
+});
 app.listen(3000);
