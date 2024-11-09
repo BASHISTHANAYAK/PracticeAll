4cfa5af8dc0a28c2a0c3934dc55cdc5d95aecad5
BASHISTHANAYAK <bashisthanayak09@gmail.com>
2024-11-10 00:53:14 +0530

    using bcrypt to bcrypt and decrypt password

--- NodeJS_In-Sheryians_Coding_School/MongoDB/Authentication-Authorization-Bcrypt-JWT-Part14/app.js
+++ NodeJS_In-Sheryians_Coding_School/MongoDB/Authentication-Authorization-Bcrypt-JWT-Part14/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 
 app.use(cookieParser());
@@ -10,10 +11,29 @@ app.get("/", function (req, res) {
   res.send("Home");
 });
 
-app.get("/readCookies", function (req, res) {
-  // for test seting cookie
-  let result = req.cookies;
-  res.send(result);
+// app.get("/readCookies", function (req, res) {
+//   // for test seting cookie
+//   let result = req.cookies;
+//   res.send(result);
+// });
+
+// to make something hashed using bcrypt..
+app.get("/bcryptPassword", function (req, res) {
+  bcrypt.hash("aDemoPassword", 12, function (err, hash) {
+    //this time hashed result is , it will change -$2b$10$QlRC1/z2KDQDdKWzCWwABO18g4B.eDr6fR9T8c5KevnC9b71LFmqK
+    res.send(hash);
+  });
+});
+
+//to make something decrypt
+app.get("/decryptPassword", function (req, res) {
+  bcrypt.compare(
+    "aDemoPassword",
+    "$2b$10$QlRC1/z2KDQDdKWzCWwABO18g4B.eDr6fR9T8c5KevnC9b71LFmqK",
+    function (err, result) {
+      res.send(result);
+    }
+  );
 });
 
 app.listen(3000, function () {
