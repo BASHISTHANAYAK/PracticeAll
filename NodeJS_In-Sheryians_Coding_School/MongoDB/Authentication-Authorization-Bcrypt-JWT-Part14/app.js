9a4a47541f40739cf643691685f89f43d9b50897
BASHISTHANAYAK <bashisthanayak09@gmail.com>
2024-11-10 01:22:48 +0530

    sign and verifying jwt token

--- NodeJS_In-Sheryians_Coding_School/MongoDB/Authentication-Authorization-Bcrypt-JWT-Part14/app.js
+++ NodeJS_In-Sheryians_Coding_School/MongoDB/Authentication-Authorization-Bcrypt-JWT-Part14/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 
 app.use(cookieParser());
@@ -17,23 +18,33 @@ app.get("/", function (req, res) {
 //   res.send(result);
 // });
 
-// to make something hashed using bcrypt..
-app.get("/bcryptPassword", function (req, res) {
-  bcrypt.hash("aDemoPassword", 12, function (err, hash) {
-    //this time hashed result is , it will change -$2b$10$QlRC1/z2KDQDdKWzCWwABO18g4B.eDr6fR9T8c5KevnC9b71LFmqK
-    res.send(hash);
-  });
+// // to make something hashed using bcrypt..
+// app.get("/bcryptPassword", function (req, res) {
+//   bcrypt.hash("aDemoPassword", 12, function (err, hash) {
+//     res.send(hash);
+//   });
+// });
+
+// //to make something decrypt
+// app.get("/decryptPassword", function (req, res) {
+//   bcrypt.compare(
+//     "aDemoPassword",
+//     "$2b$10$QlRC1/z2KDQDdKWzCWwABO18g4B.eDr6fR9T8c5KevnC9b71LFmqK",
+//     function (err, result) {
+//       res.send(result);
+//     }
+//   );
+// });
+
+// JWT
+app.get("/signInJwt", function (req, res) {
+  var token = jwt.sign({ email: "bashistha@gmail.com" }, "shhhhh");
+  res.send(token);
 });
 
-//to make something decrypt
-app.get("/decryptPassword", function (req, res) {
-  bcrypt.compare(
-    "aDemoPassword",
-    "$2b$10$QlRC1/z2KDQDdKWzCWwABO18g4B.eDr6fR9T8c5KevnC9b71LFmqK",
-    function (err, result) {
-      res.send(result);
-    }
-  );
+app.get("/verifyJwt", function (req, res) {
+    var decoded = jwt.verify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJhc2hpc3RoYUBnbWFpbC5jb20iLCJpYXQiOjE3MzExODE4ODV9.NaEFD7YS2Ck34XHLqz-GefZRqaAEOLwY4uPZvWNFvo8", 'shhhhh');
+    res.send(decoded);
 });
 
 app.listen(3000, function () {
